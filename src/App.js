c2c4f75eab77f7a6228052f1cd3c2528ea576c4e
Clyford <hclyford@gmail.com>
2023-04-05 13:51:56 -0700

    change app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/Home" element={<Home/>} />
           <Route path="/projects" element={<Projects/>} />
           <Route path="/About" element={<About/>} />
           <Route path="/education" element={<Education/>} />
